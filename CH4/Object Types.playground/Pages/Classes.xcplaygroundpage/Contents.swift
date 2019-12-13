86f73aef28e0030cbe975aee9768be997f3cd8c5
Tiffany Ho <Tam@Poohs-iMac.local>
2019-12-13 00:31:12 -0800

    adding CH4

---
+++ CH4/Object Types.playground/Pages/Classes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,89 @@
+class Dog {
+    var name : String?
+    var license: Int = 0
+    
+    init(name:String?) {
+        self.name = name
+    }
+    
+    init(license:Int) {
+        self.license = license
+    }
+    
+    convenience init(name: String?, license: Int) {
+        self.init(name: name)
+        self.init(license: license)
+    }
+
+}
+let fido = Dog(name: "Luna",license: 1234)
+
+class BetterDog {
+    var name : String
+    var license : Int
+    
+    init(_ name: String = "", _ license: Int = 0) {
+        self.name = name
+        self.license = license
+    }
+    
+    func toString() {
+        print("\(name) has the license \(license)")
+    }
+}
+
+let luna = BetterDog("Luna", 1234)
+luna.name == fido.name
+
+class optionalEx {
+    var name : String?
+}
+
+let cl = optionalEx() //This works because name is initialized to nil. Default initial values are required.
+
+//An initializer can not refer to self until all instance properties have been initialized
+
+struct Cat {
+    var name : String
+    var license: Int
+    init(name:String, license:Int) {
+        //meow() compile error called before properties are initialized
+        self.name = name
+        self.license = license
+    }
+    
+    func meow() {
+        print("meow")
+    }
+}
+
+let kitty = Cat(name: "kitty", license: 1)
+
+struct Digit {
+    var number : Int
+    var meaningOfLife : Bool //Delegating initializer cannot set a constant property
+    init(number : Int) {
+        self.number = number
+        self.meaningOfLife = false
+    }
+    
+    //Cannot refer to self until after the call to an init
+    init() { //delegating initializer
+        self.init(number: 42)
+        self.meaningOfLife = true
+    }
+}
+
+//Failable initializers: return an optional wrapping the new instance
+class FailableDoggy {
+    let name : String
+    let license : Int
+    
+    init?(name: String, license: Int) {
+        if name.isEmpty || license <= 0 {
+            return nil
+        }
+        self.name = name
+        self.license = license
+    }
+}
--- CH4/Object Types.playground/Pages/Enums.xcplaygroundpage/Contents.swift
+++ CH4/Object Types.playground/Pages/Enums.xcplaygroundpage/Contents.swift
@@ -1,1 +1,10 @@
+"""
+Object types come in three different flavors: enums, strucs, and classes
 
+What are the differences between them?
+
+How would you create your own object type?
+
+
+
+"""
